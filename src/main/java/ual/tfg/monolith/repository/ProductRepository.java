cd7effecb3e043d7118e122e340df1eadf14f2a4
dgomezl <david.parador99@gmail.com>
2023-05-30 09:07:04 +0200

    m

--- src/main/java/ual/tfg/monolith/repository/ProductRepository.java
+++ src/main/java/ual/tfg/monolith/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package ual.tfg.monolith.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ual.tfg.monolith.entity.Product;
@@ -7,9 +8,8 @@ import ual.tfg.monolith.entity.Product;
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAll();
     List<Product> findAllByCategoria(String categoria);
-
     List<Product> findByNameContainingIgnoreCase(String name);
 }
