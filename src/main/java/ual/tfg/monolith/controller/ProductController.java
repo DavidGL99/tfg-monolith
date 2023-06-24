85fc6e9f1844336e5b0850573a13a6cda33f9290
David Gomez <david.parador99@gmail.com>
2023-06-24 21:11:17 +0200

    aws

--- src/main/java/ual/tfg/monolith/controller/ProductController.java
+++ src/main/java/ual/tfg/monolith/controller/ProductController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
-@CrossOrigin(allowedHeaders = "*")
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
